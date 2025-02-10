a59d8d0a741f279ec75b5dc6c79839b5f56e3e8a
ayaz9616 <157129181+ayaz9616@users.noreply.github.com>
2025-02-10 19:03:33 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
     <div>
       <div className="home">
         <div className="home-content">
-          <h1>Welcome to our E-learning Platform</h1>
-          <p>Learn, Grow, Excel</p>
+          <h1>Welcome to our Gym</h1>
+          <p>By Tridev Pandey</p>
           <button onClick={() => navigate("/courses")} className="common-btn">
             Get Started
           </button>
